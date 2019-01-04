2d82412570680d26edecc8e6b9698b46ac50b7b9
Conor Watson <conor.watson@soar.tech>
2019-01-04 16:21:03 +0000

    Removed HelloWorld. Created env file. Pulling project repository info from Gist. Created PortfolioItem. Created Portfolio

---
+++ src/components/PortfolioItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="portfolioItem">
+    <p>{{ name }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PortfolioItem",
+  components: {},
+  props: ["itemData"],
+  computed: {
+    name() {
+      return this.itemData ? this.itemData.name : null;
+    }
+  },
+  watch: {
+    itemData() {
+      console.log(this.itemData);
+    }
+  }
+};
+</script>
---
+++ src/views/Portfolio.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="portfolioContainer">
+    <h2>Portfolio</h2>
+    <p>From Minigolf Games to Christmas Puzzle Events! Check out some of my personal projects.</p>
+    <div class="projects">
+      <portfolio-item v-for="(item, index) in portfolioItems" :key="index" :itemData="item"></portfolio-item>
+    </div>
+  </div>
+</template>
+
+<script>
+import PortfolioItem from "../components/PortfolioItem";
+
+export default {
+  name: "portfolio",
+  components: {
+    PortfolioItem
+  },
+  data() {
+    return {
+      portfolioItems: [],
+      gistID: process.env.VUE_APP_GIST_ID
+    };
+  },
+  async mounted() {
+    const res = await fetch(`https://api.github.com/gists/${this.gistID}`);
+
+    const gist = await res.json();
+    const websiteconfig = JSON.parse(gist.files["websiteconfig.json"].content);
+    this.portfolioItems = websiteconfig.repositories;
+  }
+};
+</script>
+
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css?family=Fjalla+One");
+@import url("https://fonts.googleapis.com/css?family=Roboto");
+@import "../assets/global.scss";
+
+.portfolioContainer {
+  height: 400px;
+  width: 100%;
+  background: $primaryGrey;
+  color: $bodytextGrey;
+  font-family: "Roboto", sans-serif;
+  text-align: left;
+
+  h2 {
+    margin: 0;
+    padding: 25px 0 0 25px;
+    font-family: "Fjalla One", sans-serif;
+    font-size: 32px;
+    text-transform: uppercase;
+    color: $headingGrey;
+    position: relative;
+    display: inline-block;
+
+    &:after {
+      content: " ";
+      position: absolute;
+      top: calc(100% + 6px);
+      left: 25px;
+      height: 1px;
+      width: calc(100% - 25px);
+      background: $headingGrey;
+    }
+  }
+
+  p {
+    padding: 0 25px;
+    font-size: 18px;
+  }
+}
+</style>
+
