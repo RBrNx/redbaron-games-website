989b1268f3b303a66fab673a85e3eb500207fc9e
Conor Watson <conor.watson@soar.tech>
2019-03-24 16:11:45 +0000

    Added Specialities to About page.
    Changed layout of Home css.
    Added Specialities section to the About page.
    Added SVGs for the About section.

--- src/views/About.vue
+++ src/views/About.vue
@@ -52,46 +52,47 @@
       <h2>My Specialities</h2>
       <div class="specialities">
         <div class="card">
-          <img src="../assets/WebDevelopment.png">
+          <div v-html="require('!html-loader!../assets/WebDevelopment.svg')"></div>
           <div class="title">Web Development</div>
           <div
             class="description"
-          >Watch your project come alive with great animations and intuitive features</div>
+          >With a first-class degree in Computer Science, I am able to bring your animations and interactions to life with proper software development techniques.</div>
         </div>
         <div class="card">
-          <img src="../assets/WebDevelopment.png">
-          <div class="title">Web Development</div>
+          <div v-html="require('!html-loader!../assets/Learning2.svg')"></div>
+          <div class="title">Rapid Learning</div>
           <div
             class="description"
-          >Watch your project come alive with great animations and intuitive features</div>
+          >I love to increase both my technical and real world knowledge, picking up new skills along the way. I have found that I am able to do this fairly rapidly.</div>
         </div>
         <div class="card">
-          <img src="../assets/WebDevelopment.png">
-          <div class="title">Web Development</div>
+          <div v-html="require('!html-loader!../assets/MobileDevelopment2.svg')"></div>
+          <div class="title">Native Mobile Development</div>
           <div
             class="description"
-          >Watch your project come alive with great animations and intuitive features</div>
+          >A great native experience can really enhance the success and professionality of a project.</div>
         </div>
         <div class="card">
-          <img src="../assets/WebDevelopment.png">
-          <div class="title">Web Development</div>
-          <div
-            class="description"
-          >Watch your project come alive with great animations and intuitive features</div>
+          <div v-html="require('!html-loader!../assets/ProblemSolving.svg')"></div>
+          <div class="title">Problem Solving</div>
+          <div class="description">
+            Problem solving shouldn't be about finding the quickest and easiet workaround, it's about finding the
+            <em>right</em> solution to your problem.
+          </div>
         </div>
         <div class="card">
-          <img src="../assets/WebDevelopment.png">
-          <div class="title">Web Development</div>
+          <div v-html="require('!html-loader!../assets/ResponsiveWebDesign.svg')"></div>
+          <div class="title">Responsive Web Design</div>
           <div
             class="description"
-          >Watch your project come alive with great animations and intuitive features</div>
+          >While I'm no graphic designer, I do have a eye for great design. I am comfortable using design tools and can take responsive designs from mock-up to implementation.</div>
         </div>
         <div class="card">
-          <img src="../assets/WebDevelopment.png">
-          <div class="title">Web Development</div>
+          <div v-html="require('!html-loader!../assets/Teamwork.svg')"></div>
+          <div class="title">Team Collabortation</div>
           <div
             class="description"
-          >Watch your project come alive with great animations and intuitive features</div>
+          >I am a strong team player with a belief that communication is key to a successful project. I have the flexibility to adapt to where my work needs me most.</div>
         </div>
       </div>
     </div>
@@ -124,164 +125,182 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 @import url("https://fonts.googleapis.com/css?family=Fjalla+One");
 @import url("https://fonts.googleapis.com/css?family=Roboto");
 @import "../assets/global.scss";
 
-#preload {
-  display: none;
-}
-
-.heroImage {
-  background-size: cover;
-  position: relative;
-  transition: background-image 1.5s;
-
-  &:before {
-    background: linear-gradient(
-      to bottom,
-      rgba(0, 0, 0, 0.85),
-      rgba(0, 0, 0, 0.7)
-    );
-    content: "";
-    position: absolute;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
+.about {
+  #preload {
+    display: none;
   }
 
-  @include desktop {
-    min-height: 100vh;
-  }
-
-  #particles-js {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-  }
-
-  .heroText {
-    font-family: "Fjalla One", sans-serif;
-    color: #fff;
+  .heroImage {
+    background-size: cover;
     position: relative;
-    text-transform: uppercase;
-    font-size: 24px;
-    width: 100%;
-    padding: 100px 15%;
-    top: 15%;
+    transition: background-image 1.5s;
+
+    &:before {
+      background: linear-gradient(
+        to bottom,
+        rgba(0, 0, 0, 0.85),
+        rgba(0, 0, 0, 0.7)
+      );
+      content: "";
+      position: absolute;
+      top: 0;
+      right: 0;
+      bottom: 0;
+      left: 0;
+    }
 
     @include desktop {
+      min-height: 100vh;
+    }
+
+    #particles-js {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+    }
+
+    .heroText {
+      font-family: "Fjalla One", sans-serif;
+      color: #fff;
+      position: relative;
+      text-transform: uppercase;
       font-size: 24px;
-      top: 50%;
-      transform: translateY(-50%);
-      padding: 0 20%;
-      position: absolute;
-    }
+      width: 100%;
+      padding: 100px 15%;
+      top: 15%;
 
-    .title {
-      margin-bottom: 150px;
-      font-size: 45px;
-
-      h2 {
-        margin: 0;
-      }
-    }
-
-    .subtitle {
-      display: flex;
-      align-items: center;
-      flex-direction: column;
-      font-family: Roboto;
-      line-height: 40px;
-    }
-
-    .highlight {
-      color: $primaryRed;
-    }
-  }
-}
-
-.specialitiesContainer {
-  width: 100%;
-  background: $primaryGrey;
-  color: $bodytextGrey;
-  font-family: "Roboto", sans-serif;
-  text-align: left;
-  padding-bottom: 50px;
-
-  h2 {
-    margin: 0;
-    padding: 25px 0 0 25px;
-    font-family: "Fjalla One", sans-serif;
-    font-size: 32px;
-    text-transform: uppercase;
-    color: $headingGrey;
-    position: relative;
-    display: inline-block;
-
-    &:after {
-      content: " ";
-      position: absolute;
-      top: calc(100% + 6px);
-      left: 25px;
-      height: 1px;
-      width: calc(100% - 25px);
-      background: $headingGrey;
-    }
-  }
-
-  .specialities {
-    padding: 0 25px;
-    position: relative;
-
-    display: grid;
-    grid-template-columns: repeat(1, 1fr);
-    grid-gap: 30px;
-
-    @include tablet {
-      grid-template-columns: repeat(2, 2fr);
-    }
-
-    @include desktop {
-      grid-template-columns: repeat(3, 2fr);
-    }
-
-    .card {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-
-      img {
-        height: 200px;
-        margin-bottom: 15px;
+      @include desktop {
+        font-size: 24px;
+        top: 50%;
+        transform: translateY(-50%);
+        padding: 0 20%;
+        position: absolute;
       }
 
       .title {
-        font-family: "Fjalla One", sans-serif;
-        font-size: 20px;
-        color: $headingGrey;
-        margin-bottom: 20px;
-        position: relative;
+        margin-bottom: 150px;
+        font-size: 45px;
 
-        &:after {
-          content: "";
-          display: block;
-          height: 2px;
-          width: 70px;
-          background-color: $primaryRed;
-          left: 50%;
-          top: 10px;
-          position: relative;
-          transform: translateX(-50%);
+        h2 {
+          margin: 0;
         }
       }
 
-      .description {
-        text-align: center;
+      .subtitle {
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        font-family: Roboto;
+        line-height: 40px;
+      }
+
+      .highlight {
+        color: $primaryRed;
+      }
+    }
+  }
+
+  .specialitiesContainer {
+    width: 100%;
+    background: $primaryGrey;
+    color: $bodytextGrey;
+    font-family: "Roboto", sans-serif;
+    text-align: left;
+    padding-bottom: 50px;
+
+    h2 {
+      margin: 0;
+      margin-bottom: 18px;
+      padding: 25px 0 0 25px;
+      font-family: "Fjalla One", sans-serif;
+      font-size: 32px;
+      text-transform: uppercase;
+      color: $headingGrey;
+      position: relative;
+      display: inline-block;
+
+      &:after {
+        content: " ";
+        position: absolute;
+        top: calc(100% + 6px);
+        left: 25px;
+        height: 1px;
+        width: calc(100% - 25px);
+        background: $headingGrey;
+      }
+    }
+
+    .specialities {
+      padding: 0 25px;
+      position: relative;
+
+      display: grid;
+      grid-template-columns: repeat(1, 1fr);
+      grid-gap: 30px;
+
+      @include tablet {
+        grid-template-columns: repeat(2, 2fr);
+      }
+
+      @include desktop {
+        grid-template-columns: repeat(3, 2fr);
+      }
+
+      .card {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        background: lighten($primaryGrey, 5%);
+        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+        border-radius: 5px;
+        padding-left: 10%;
+        padding-right: 10%;
+        padding-top: 5%;
+        padding-bottom: 5%;
+        transition: all 0.3s;
+
+        &:hover {
+          transform: scale(1.02);
+        }
+
+        img,
+        svg {
+          height: 100px;
+          width: 100px;
+          margin-bottom: 15px;
+          fill: $primaryRed;
+        }
+
+        .title {
+          font-family: "Fjalla One", sans-serif;
+          font-size: 24px;
+          color: $headingGrey;
+          margin-bottom: 40px;
+          position: relative;
+
+          &:after {
+            content: "";
+            display: block;
+            height: 2px;
+            width: 70px;
+            background-color: $primaryRed;
+            left: 50%;
+            top: 10px;
+            position: relative;
+            transform: translateX(-50%);
+          }
+        }
+
+        .description {
+          text-align: center;
+        }
       }
     }
   }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -61,139 +61,141 @@ export default {
 @import url("https://fonts.googleapis.com/css?family=Roboto");
 @import "../assets/global.scss";
 
-.heroImage {
-  background: url("../assets/HeroHeader2.jpg") no-repeat center;
-  background-size: cover;
-  position: relative;
-
-  &:before {
-    background: linear-gradient(
-      to bottom,
-      rgba(0, 0, 0, 0.7),
-      rgba(0, 0, 0, 0.9)
-    );
-    content: "";
-    position: absolute;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-  }
-
-  @include desktop {
-    min-height: 100vh;
-  }
-
-  #particles-js {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-  }
-
-  .heroText {
-    font-family: "Fjalla One", sans-serif;
-    color: #fff;
+.home {
+  .heroImage {
+    background: url("../assets/HeroHeader2.jpg") no-repeat center;
+    background-size: cover;
     position: relative;
-    text-transform: uppercase;
-    font-size: 24px;
-    width: 100%;
-    padding: 100px 15%;
-    top: 15%;
+
+    &:before {
+      background: linear-gradient(
+        to bottom,
+        rgba(0, 0, 0, 0.7),
+        rgba(0, 0, 0, 0.9)
+      );
+      content: "";
+      position: absolute;
+      top: 0;
+      right: 0;
+      bottom: 0;
+      left: 0;
+    }
 
     @include desktop {
-      font-size: 24px;
-      top: 50%;
-      transform: translateY(-50%);
-      padding: 0 20%;
+      min-height: 100vh;
+    }
+
+    #particles-js {
       position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
     }
 
-    .title {
-      margin-bottom: 150px;
-      font-size: 45px;
+    .heroText {
+      font-family: "Fjalla One", sans-serif;
+      color: #fff;
+      position: relative;
+      text-transform: uppercase;
+      font-size: 24px;
+      width: 100%;
+      padding: 100px 15%;
+      top: 15%;
 
-      h2 {
-        margin: 0;
+      @include desktop {
+        font-size: 24px;
+        top: 50%;
+        transform: translateY(-50%);
+        padding: 0 20%;
+        position: absolute;
       }
-    }
 
-    .subtitle {
-      display: flex;
-      align-items: center;
-      flex-direction: column;
-      font-family: Roboto;
+      .title {
+        margin-bottom: 150px;
+        font-size: 45px;
 
-      .subtitleWords {
-        //height: 50px;
-        margin-top: 15px;
-        position: relative;
-        width: 100%;
+        h2 {
+          margin: 0;
+        }
+      }
 
-        span {
-          display: block;
-          position: absolute;
+      .subtitle {
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        font-family: Roboto;
+
+        .subtitleWords {
+          //height: 50px;
+          margin-top: 15px;
+          position: relative;
           width: 100%;
-          opacity: 0;
-          overflow: hidden;
-          text-align: center;
-          animation: rotateWords 20s linear infinite 0s;
 
-          &:nth-child(2) {
-            animation-delay: 4s;
-          }
-          &:nth-child(3) {
-            animation-delay: 8s;
-          }
-          &:nth-child(4) {
-            animation-delay: 12s;
-          }
-          &:nth-child(5) {
-            animation-delay: 16s;
-          }
-          &:nth-child(6) {
-            animation-delay: 20s;
+          span {
+            display: block;
+            position: absolute;
+            width: 100%;
+            opacity: 0;
+            overflow: hidden;
+            text-align: center;
+            animation: rotateWords 20s linear infinite 0s;
+
+            &:nth-child(2) {
+              animation-delay: 4s;
+            }
+            &:nth-child(3) {
+              animation-delay: 8s;
+            }
+            &:nth-child(4) {
+              animation-delay: 12s;
+            }
+            &:nth-child(5) {
+              animation-delay: 16s;
+            }
+            &:nth-child(6) {
+              animation-delay: 20s;
+            }
           }
         }
       }
-    }
 
-    .highlight {
-      color: $primaryRed;
+      .highlight {
+        color: $primaryRed;
+      }
     }
   }
-}
 
-@keyframes rotateWords {
-  0% {
-    opacity: 0;
-  }
-  2% {
-    opacity: 0;
-    -webkit-transform: translateY(-30px);
-    transform: translateY(-30px);
-  }
-  8% {
-    opacity: 1;
-    -webkit-transform: translateY(0px);
-    transform: translateY(0px);
-  }
-  18% {
-    opacity: 1;
-    -webkit-transform: translateY(0px);
-    transform: translateY(0px);
-  }
-  22% {
-    opacity: 0;
-    -webkit-transform: translateY(30px);
-    transform: translateY(30px);
-  }
-  80% {
-    opacity: 0;
-  }
-  100% {
-    opacity: 0;
+  @keyframes rotateWords {
+    0% {
+      opacity: 0;
+    }
+    2% {
+      opacity: 0;
+      -webkit-transform: translateY(-30px);
+      transform: translateY(-30px);
+    }
+    8% {
+      opacity: 1;
+      -webkit-transform: translateY(0px);
+      transform: translateY(0px);
+    }
+    18% {
+      opacity: 1;
+      -webkit-transform: translateY(0px);
+      transform: translateY(0px);
+    }
+    22% {
+      opacity: 0;
+      -webkit-transform: translateY(30px);
+      transform: translateY(30px);
+    }
+    80% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 0;
+    }
   }
 }
 </style>
