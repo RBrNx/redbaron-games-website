8397b599fe4b83a1244dfe240e29f2f9b7da43c4
Conor Watson <conor.watson@soar.tech>
2019-07-21 16:13:41 +0100

    feat: Created MarkdownRenderer.vue

---
+++ src/components/MarkdownRenderer.vue
@@ -0,0 +1,16 @@
+<template>
+  <div v-html="compiledMarkdown"></div>
+</template>
+
+<script>
+import marked from "marked";
+
+export default {
+  props: ["source"],
+  computed: {
+    compiledMarkdown: function() {
+      return marked(this.source, { sanitize: true, breaks: true });
+    }
+  }
+};
+</script>
