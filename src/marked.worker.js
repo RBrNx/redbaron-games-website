89efbec68c1ef0ed62b61e1e892f98fa9eab7aa5
Conor Watson <conor.watson@soar.tech>
2019-08-03 14:46:04 +0100

    feat: Added fancyLink rendering to Marked worker

--- src/marked.worker.js
+++ src/marked.worker.js
@@ -1,8 +1,14 @@
 import marked from 'marked';
 
 onmessage = e => {
+  const renderer = new marked.Renderer();
+
+  renderer.link = (href, title, text) => {
+    return `<span class="fancyLink"><a href="${href}" target="_blank" rel="noreferrer">${text}</a></span>`;
+  };
+
   const markdownString = e.data;
-  const HTML = marked(markdownString);
+  const HTML = marked(markdownString, { renderer: renderer });
 
   self.postMessage(HTML);
 };
